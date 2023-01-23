5bbefc1cc1916d753765b480f34416768444281a
Ali Guliyev <54935778+Ali-Guliyev@users.noreply.github.com>
2023-01-23 23:13:00 +0300

    update

--- game.py
+++ game.py
@@ -12,7 +12,7 @@ pygame.display.set_caption("Space Invaders")
 pygame.display.set_icon(pygame.image.load(f"{path}assets/images/icon.ico"))
 clock = pygame.time.Clock()
 enemy_die_time = 0
-enemy_bullet_time = 300
+enemy_bullet_time = 400
 enemy_bullet_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(enemy_bullet_timer, enemy_bullet_time)
 
@@ -21,7 +21,7 @@ def playsound(src, volume = 1, times = 0):
   sound.set_volume(volume)
   pygame.mixer.find_channel().play(sound, times)
 
-def playmusic(src, times = -1):
+def playBgMusic(src, times = -1):
   pygame.mixer.music.load(src)
   pygame.mixer.music.play(times)
 
@@ -74,10 +74,10 @@ class Game:
   def restart(self):
     self.score = 0
     reposition_enemies()
-    player.position()
-    player_bullet.position()
+    player.restart()
+    player_bullet.restart()
     reposition_obstacles()
-    playmusic(f"{path}assets/sounds/background-music.mp3")
+    playBgMusic(f"{path}assets/sounds/background-music.mp3")
     self.over_count = 0
 
   def over(self):
@@ -129,12 +129,14 @@ class Player(Sprite):
   def __init__(self, x, y):
     self.speed = 3
     self.image = f"{path}assets/images/player/player.png"
+    self.health = 1
     super().__init__(x, y, self.image, self.speed)
 
   def update(self):
     super().update()
 
     self.movement()
+    self.check_health()
     if self.sprite_rect.left <= 0:
       self.sprite_rect.left = 0
     elif self.sprite_rect.right > WIDTH:
@@ -147,8 +149,15 @@ class Player(Sprite):
     elif keys[pygame.K_RIGHT]:
       self.sprite_rect.x += self.speed
 
-  def position(self):
+  def check_health(self):
+    for i in range(self.health):
+      heart_sprite = Sprite(80 + i * 50, HEIGHT + 14, f"{path}assets/images/heart.png")
+      heart_sprite.transform(0.4)
+      heart_sprite.update()
+
+  def restart(self):
     self.sprite_rect.centerx = WIDTH / 2
+    self.health = 1
 
 class Obstacle(Sprite):
   def __init__(self, x, y, image):
@@ -209,10 +218,10 @@ class PlayerBullet(Bullet):
   def shoot(self):
     if not self.isShooting:
       super().shoot()
-      self.position()
+      self.restart()
       playsound(f"{path}assets/sounds/laser.mp3", 0.6)
 
-  def position(self):
+  def restart(self):
     self.sprite_rect.centerx = player.sprite_rect.centerx
     self.sprite_rect.y = player.sprite_rect.top - 5
 
@@ -231,11 +240,13 @@ class EnemyBullet(Bullet):
       self.sprite_rect.x = 0
       self.sprite_rect.y = 0
 
-    # Game over
+    # Game over (Enemy Bullet hits the Player)
     if self.sprite_rect.colliderect(player.sprite_rect):
+      player.health -= 1
       self.sprite_rect.x = 0
       self.sprite_rect.y = 0
-      game.over()
+      if player.health == 0:
+        game.over()
 
     if self.sprite_rect.y >= HEIGHT:
       self.isShooting = False
@@ -243,6 +254,7 @@ class EnemyBullet(Bullet):
   def shoot(self):
     if not self.isShooting:
       super().shoot()
+
       random_enemy = random.choice(enemies)
       self.sprite_rect.centerx = random_enemy.sprite_rect.centerx
       self.sprite_rect.y = random_enemy.sprite_rect.bottom
@@ -251,9 +263,9 @@ class EnemyBullet(Bullet):
 class Enemy(Sprite):
   def __init__(self, x, y):
     self.speed = 1
-    self.down_speed = 40
+    self.go_down_pixels = 40
     self.image = f"{path}assets/images/enemy/enemy.png"
-    self.status = "enemy"
+    self.status = "alive"
     super().__init__(x, y, self.image, self.speed)
 
   def update(self):
@@ -265,6 +277,10 @@ class Enemy(Sprite):
     if self.status != "exploded" and self.sprite_rect.colliderect(player_bullet.sprite_rect) and player_bullet.isShooting:
       player_bullet.isShooting = False
       game.score += 10
+      # High score
+      if game.score > game.high_score:
+        game.high_score = game.score
+
       enemy_bullet_time += 20
       pygame.time.set_timer(enemy_bullet_timer, enemy_bullet_time)
       playsound(f"{path}assets/sounds/explosion.mp3", 0.6)
@@ -288,21 +304,36 @@ class Enemy(Sprite):
 
     self.sprite_rect.x += self.speed
 
+class PickUpHeart(Sprite):
+  def __init__(self, x, y):
+    self.image = f"{path}assets/images/heart.png"
+    self.speed = 0
+    self.angle = 0
+    self.rotSpeed = 2
+    super().__init__(x, y, self.image, self.speed)
+    self.transform(0.5)
+
+  def update(self):
+    # Rotating
+    self.angle += 6
+    img_copy = pygame.transform.rotate(self.sprite_surf, self.angle)
+    screen.blit(img_copy, (self.sprite_rect.x - int(img_copy.get_width() / 2), self.sprite_rect.y - int(img_copy.get_height() / 2)))
+
+  # Game
 game = Game()
 
 # Surfaces
 background_surf = pygame.image.load(f"{path}assets/images/background.jpg").convert()
 
+# Player
 player = Player(WIDTH / 2, 700)
 player_bullet = PlayerBullet()
 
-score_text = Text(" ", 100, 50, 50, game.colors["primary"])
+# Texts
+score_text = Text("", 100, 50, 50, game.colors["primary"])
 high_score_text = Text("", WIDTH - 150, 50, 50, game.colors["primary"])
 
 # Obstacles
-# obstacle = Sprite(100, HEIGHT - 170, "assets/images/obstacle.png")
-# obstacle.transform(0.3)
-
 obstacles = [[], [], []]
 reposition_obstacles()
 
@@ -316,6 +347,9 @@ enemy_bullets = []
 # Enemies
 enemies = []
 reposition_enemies()
+
+# Pick Up Heart
+pickUpHeart = PickUpHeart(WIDTH / 2, HEIGHT / 2)
 
 while True:
   for event in pygame.event.get():
@@ -336,8 +370,6 @@ while True:
   screen.blit(background_surf, (0, 0))
 
   if game.isActive:
-    # pygame.mixer.music.unpause()
-
     # Scores
     score_text.update()
     score_text.text = f"Score: {game.score}"
@@ -365,15 +397,20 @@ while True:
       if enemy.sprite_rect.right >= WIDTH or enemy.sprite_rect.left <= 0:
         for enemy in enemies:
           enemy.speed = -enemy.speed
-          enemy.sprite_rect.y += enemy.down_speed
+          enemy.sprite_rect.y += enemy.go_down_pixels
 
     # Death Borders
     bound1.update()
     bound2.update()
 
-  else:
-    # pygame.mixer.music.pause()
+    # Pick Up Heart
+    pickUpHeart.update()
 
+    # pickUpHeart.rotate += 1
+    # pickUpHeart.transform(pickUpHeart.scale, pickUpHeart.rotate)
+
+
+  else:
     title_text = Text("", WIDTH / 2, HEIGHT / 2 - 100, 70, game.colors["player_blue"])
 
     invader_img = Sprite(WIDTH / 2 + 30, HEIGHT / 2 + 30, f"{path}assets/images/player/player.png")
@@ -383,22 +420,24 @@ while True:
     press_space_text.update()
 
     score_text_number = Text("", WIDTH / 2, HEIGHT / 2 + 150, 50, game.colors["player_blue"])
+    high_score_text_number = Text("", WIDTH / 2, HEIGHT / 2 + 190, 50, game.colors["player_blue"])
 
     if game.attempts == 0:
       title_text.text = "Space Invaders"
     else:
       # Game Over Screen
-
       game.over_count += 1
 
       if (game.over_count == 2):
         print(game.over_count)
-        playmusic(f"{path}assets/sounds/lose.mp3");
+        playBgMusic(f"{path}assets/sounds/lose.mp3");
 
       title_text.text = "Game Over"
       score_text_number.text = f"Score: {game.score}"
+      high_score_text_number.text = f"High Score: {game.high_score}"
 
     score_text_number.update()
+    high_score_text_number.update()
     invader_img.update()
     title_text.update()
 
